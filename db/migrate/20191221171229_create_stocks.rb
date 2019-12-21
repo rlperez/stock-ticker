3a952a3a7e5708231bca87bd72caf38b8300065b
Rigoberto L. Perez <rlperez@udel.edu>
2019-12-21 12:47:23 -0500

    Longest symbol seems to be 5 but extra room for cusion

--- db/migrate/20191221171229_create_stocks.rb
+++ db/migrate/20191221171229_create_stocks.rb
@@ -1,7 +1,7 @@
 class CreateStocks < ActiveRecord::Migration[6.0]
   def change
     create_table :stocks do |t|
-      t.string :symbol
+      t.string :symbol, limit: 10
       t.string :company_name
       t.string :exchange
       t.string :industry
