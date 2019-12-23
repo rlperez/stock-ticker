03416192b0fd73490904e42e15df8e2a809659e5
Rigoberto L. Perez <rlperez@udel.edu>
2019-12-23 15:52:26 -0500

    Add table of stock data...only 5 days at this point due to api limits

--- app/controllers/stock_controller.rb
+++ app/controllers/stock_controller.rb
@@ -1,5 +1,8 @@
 class StockController < ApplicationController
+  @@client = IEX::Api::Client.new
+
   def show
     @stock = Stock.find(params[:id])
+    @chart_data = @@client.chart(@stock.symbol, '5d', chart_interval: 1)
   end
 end
