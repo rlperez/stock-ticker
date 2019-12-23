1c8b7076e7fc3777469ddf3e32a11b9dc67214dd
Rigoberto L. Perez <rlperez@udel.edu>
2019-12-23 14:34:14 -0500

    Update to have drill down logic for specific stocks

--- app/controllers/stock_controller.rb
+++ app/controllers/stock_controller.rb
@@ -1,2 +1,5 @@
 class StockController < ApplicationController
+  def show
+    @stock = Stock.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
-
   root 'home#index'
-
   post 'home/add_stock'
+  resources :stock, param: :id
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
